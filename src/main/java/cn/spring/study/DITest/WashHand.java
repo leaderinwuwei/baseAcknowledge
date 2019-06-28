d3ceea499ea20c30c6a94a0239e181f6a1551dd4
wangfeng <wangfeng@cnstrong.cn>
2019-06-28 17:19:38 +0800

    AOP commit

---
+++ src/main/java/cn/spring/study/DITest/WashHand.java
@@ -0,0 +1,12 @@
+package cn.spring.study.DITest;
+
+/**
+ * 2 * @Author: WF
+ * 3 * @Date: 2019/6/28/028 16:30
+ * 4
+ */
+public class WashHand {
+    public void attention(){
+        System.out.println("吃东西之前需要洗手");
+    }
+}
